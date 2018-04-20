c62475a3355385bf1b7946294a9cbba2ef53627b
Ahmad <ahmad.almekhlafy@gmail.com>
2018-04-20 23:51:44 +0200

    Programmieren1: Blatt1.

---
+++ Python/NoSwitchCase.py
@@ -0,0 +1,10 @@
+print("Fuer Gegenteil von hoch geben Sie 1 ein, von klein 2, und von stark 3:")
+cases = int(input())
+if cases == 1:
+	print("tief")
+elif cases ==2:
+	print("gross")
+elif cases==3:
+	print("schwach")
+else:
+	print("Error!")
---
+++ Python/Tashenrechner.py
@@ -0,0 +1,17 @@
+print ("Bitte geben Sie das erste Zahl ein:")
+Zahl1 = int(input()) 
+print ("Bitte geben Sie das zweite Zahl ein:")
+
+Zahl2 = int(input()) 
+print ("Bitte geben Sie ein Operator ein:")
+Operator = input()
+if Operator == "+" :
+	print(Zahl1 , "+" , Zahl2 , " = " , (Zahl1 + Zahl2))
+elif Operator == "-" :
+	print(Zahl1 , "-" , Zahl2 , " = " , (Zahl1 - Zahl2))
+elif Operator == "*" :
+	print(Zahl1 , "*" , Zahl2 , " = " , (Zahl1 * Zahl2))
+elif Operator == "/" :
+	print(Zahl1 , "/" , Zahl2 , " = " , (Zahl1 / Zahl2))
+else: 
+	print("Error: Operator nicht erkannt!")            
