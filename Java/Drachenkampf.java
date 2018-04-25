743b6bbaab5931f9443c7c118680d56e946bf164
Ahmad <ahmad.almekhlafy@gmail.com>
2018-04-25 23:30:08 +0200

    Drachenkampf in Java fertig!

--- Java/Drachenkampf.java
+++ Java/Drachenkampf.java
@@ -20,26 +20,41 @@ class  Drachenkampf {
 		while (Spiel.hLebenspunkte > 0 && Spiel.dLebenspunkte > 0  ) {	// Das Spiel endet, wenn die Lebenspunkte einer Partei 0 erreicht.
 
 			System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
-			int Waffe;
-
 
 			/**
-			 * Die folgende Schleife zwingt den Nützer, entweder 1 oder 2 einzugeben.
+			 * Folgendes sind zwei Weisen, um den Nutzer zu zwingen,
+			 * entweder 1 oder 2 einzugeben, kein Text und keine andere Zahlen.
 			 */
+			
+
+			// Die erste, und einfachere Weise.
+			 
+			String Input = sc.nextLine();
+			while ( !Input.equals("1") && !Input.equals("2")) {
+				System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
+				Input = sc.nextLine();
+			}
+			int Waffe = Integer.parseInt(Input);
+			
+			
+			/* Und hier ist die zweite, die eigentlich nicht genutzt wird, da sie ein bisschen komliziert ist.
+
+			int Waffe;
 			do {
 				while (!sc.hasNextInt()) {
 					sc.next();
 					System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
 				}
-				
+
 				Waffe = sc.nextInt();
 				if (Waffe != 1 && Waffe != 2) {
 					System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
 				}
 			} while ( Waffe != 1 && Waffe != 2 );
+			 */
+			
 
-
-			if (Waffe == 1) { // Wenn der Nützer 1 eingibt, attackiert der Held mit Pfeil und Bogen und reagiert der Drache. 
+			if (Waffe == 1) { // Wenn der Nützer 1 eingibt, attackiert der Held mit Pfeil und Bogen und reagiert der Drache.
 				if (Spiel.ZufallGen(Spiel.PfeilBogenZufall) >= 35) {
 					System.out.println("Der Drache wurde getroffen.");
 					Spiel.dLebenspunkte -= Spiel.PfeilBogen;
@@ -64,7 +79,7 @@ class  Drachenkampf {
 					Spiel.Status();
 				}
 
-			} else if (Waffe == 2) { // Wenn der Nützer 2 eingibt, attackiert der Held mit Schwert und reagiert der Drache. 
+			} else if (Waffe == 2) { // Wenn der Nützer 2 eingibt, attackiert der Held mit Schwert und reagiert der Drache.
 				if (Spiel.ZufallGen(Spiel.SchwertZufall) >= 35) {
 					System.out.println("Der Drache wurde getroffen.");
 					Spiel.dLebenspunkte -= Spiel.Schwert;
@@ -91,9 +106,7 @@ class  Drachenkampf {
 
 		}
 
-		/**
-		 * Das Ergibnis des Kampfs.
-		 */
+		// Das Ergibnis des Kampfs.
 		if (Spiel.dLebenspunkte <= 0 && Spiel.hLebenspunkte <= 0 ) {
 			System.out.println("Weder der Drache noch der Held konnte die Schlacht überleben.");
 		} else if (Spiel.hLebenspunkte <= 0 ) {
@@ -101,8 +114,6 @@ class  Drachenkampf {
 		} else {
 			System.out.println("Der Drache wurde besiegt. Hurra.");
 		}
-
-		
 	}
 
 
@@ -121,9 +132,7 @@ class  Drachenkampf {
 			hZaehler--;
 		}
 
-		System.out.println();
-
-		System.out.print("Leben des Drachen : ");
+		System.out.print("\nLeben des Drachen : ");
 		while (dZaehler > 0) {
 			System.out.print("X ");
 			dZaehler--;
