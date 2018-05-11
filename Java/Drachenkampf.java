bd0fed8655ee459c097b784699ecbad7e4176f5d
Ahmad Al-Mekhlafy <ahmad.almekhlafy@gmail.com>
2018-05-11 22:27:06 +0200

    First Version with Minor Editions

--- Java/Drachenkampf.java
+++ Java/Drachenkampf.java
@@ -1,153 +1,117 @@
-// April 2018
 import java.util.Scanner;
-class  Drachenkampf {
 
-	/**
-	 * Hier werden die Anfangslebenspunkte,
-	 * Genauigkeit von Waffen und ihre Angriffswerte bestimmt.
-	 */
-	int dLebenspunkte = 10;
-	int hLebenspunkte = 10;
-	int PfeilBogen = 3; int PfeilBogenZufall = 70;
-	int Schwert = 2; int SchwertZufall = 85;
-	int Feuer = 4; int FeuerZufall = 50;
+/**
+ * @author Ahmad Al-Mekhlafy 4934736 Gruppe 99
+ * @author Abdullah Nasser 4933504 Gruppe 99
+ */
+class Drachenkampf {
+
+	// Hier werden die Anfangslebenspunkte, Genauigkeit von waffen und ihre Angriffswerte bestimmt.
+	private static int dLebenspunkte = 10;
+	private static int hLebenspunkte = 10;
+	private static int pfeilBogen = 3;
+	private static int pfeilBogenZufall = 70;
+	private static int schwert = 2;
+	private static int schwertZufall = 85;
+	private static int feuer = 4;
+	private static int feuerZufall = 50;
 
 	public static void main(String[] args) {
-		Drachenkampf Spiel = new Drachenkampf();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Der Kampf beginnt");
-		Spiel.Status();
+		status();
 
-		while (Spiel.hLebenspunkte > 0 && Spiel.dLebenspunkte > 0  ) {	// Das Spiel endet, wenn die Lebenspunkte einer Partei 0 erreicht.
-
-			System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
+		while (hLebenspunkte > 0 && dLebenspunkte > 0  ) {	// Das Spiel endet, wenn die Lebenspunkte einer Partei 0 erreicht.
+			System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem schwert (2) angreifen.");
 
 			/**
 			 * Folgendes sind zwei Weisen, um den Nutzer zu zwingen,
 			 * entweder 1 oder 2 einzugeben, kein Text und keine andere Zahlen.
 			 */
-			
 
 			// Die erste, und einfachere Weise.
-			 
-			String Input = sc.nextLine();
-			while ( !Input.equals("1") && !Input.equals("2")) {
-				System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
-				Input = sc.nextLine();
+			String input = sc.nextLine();
+			while ( !input.equals("1") && !input.equals("2")) {
+				System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem schwert (2) angreifen.");
+				input = sc.nextLine();
 			}
-			int Waffe = Integer.parseInt(Input);
-			
-			
-			/* Und hier ist die zweite, die eigentlich nicht genutzt wird, da sie ein bisschen komliziert ist.
+			int waffe = Integer.parseInt(input);
 
-			int Waffe;
+			/* Und hier ist die zweite, die eigentlich nicht genutzt wird, da sie ein bisschen komliziert ist.
+			int waffe;
 			do {
 				while (!sc.hasNextInt()) {
 					sc.next();
-					System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
+					System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem schwert (2) angreifen.");
 				}
 
-				Waffe = sc.nextInt();
-				if (Waffe != 1 && Waffe != 2) {
-					System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem Schwert (2) angreifen.");
+				waffe = sc.nextInt();
+				if (waffe != 1 && waffe != 2) {
+					System.out.println("Der Held kann mit Pfeil und Bogen (1) oder mit dem schwert (2) angreifen.");
 				}
-			} while ( Waffe != 1 && Waffe != 2 );
+			} while ( waffe != 1 && waffe != 2 );
 			 */
-			
 
-			if (Waffe == 1) { // Wenn der Nützer 1 eingibt, attackiert der Held mit Pfeil und Bogen und reagiert der Drache.
-				if (Spiel.ZufallGen(Spiel.PfeilBogenZufall) >= 35) {
+			if (waffe == 1) { // Wenn der Nützer 1 eingibt, attackiert der Held mit Pfeil und Bogen und reagiert der Drache.
+				if (zufallGen(pfeilBogenZufall) >= 35) {
 					System.out.println("Der Drache wurde getroffen.");
-					Spiel.dLebenspunkte -= Spiel.PfeilBogen;
-
-					if (Spiel.ZufallGen(Spiel.FeuerZufall) >= 35) {
-						System.out.println("Der Held wurde verletzt.");
-						Spiel.hLebenspunkte -= Spiel.Feuer;
-					} else {
-						System.out.println("Der Drache hatte eine Fehlz\u00FCndung. Gl\u00FCck gehabt.");
-					}
-					Spiel.Status();
-
+					dLebenspunkte -= pfeilBogen;
 				} else {
 					System.out.println("Der Drache wurde verfehlt.");
-
-					if (Spiel.ZufallGen(Spiel.FeuerZufall) >= 35) {
-						System.out.println("Der Held wurde verletzt.");
-						Spiel.hLebenspunkte -= Spiel.Feuer;
-					} else {
-						System.out.println("Der Drache hatte eine Fehlz\u00FCndung. Gl\u00FCck gehabt.");
-					}
-					Spiel.Status();
 				}
-
-			} else if (Waffe == 2) { // Wenn der Nützer 2 eingibt, attackiert der Held mit Schwert und reagiert der Drache.
-				if (Spiel.ZufallGen(Spiel.SchwertZufall) >= 35) {
+			} else if (waffe == 2) { // Wenn der Nützer 2 eingibt, attackiert der Held mit schwert und reagiert der Drache.
+				if (zufallGen(schwertZufall) >= 35) {
 					System.out.println("Der Drache wurde getroffen.");
-					Spiel.dLebenspunkte -= Spiel.Schwert;
-
-					if (Spiel.ZufallGen(Spiel.FeuerZufall) >= 35) {
-						System.out.println("Der Held wurde verletzt.");
-						Spiel.hLebenspunkte -= Spiel.Feuer;
-					} else {
-						System.out.println("Der Drache hatte eine Fehlz\u00FCndung. Gl\u00FCck gehabt.");
-					}
-					Spiel.Status();
-
+					dLebenspunkte -= schwert;
 				} else {
 					System.out.println("Der Drache wurde verfehlt.");
-					if (Spiel.ZufallGen(Spiel.FeuerZufall) >= 35) {
-						System.out.println("Der Held wurde verletzt.");
-						Spiel.hLebenspunkte -= Spiel.Feuer;
-					} else {
-						System.out.println("Der Drache hatte eine Fehlz\u00FCndung. Gl\u00FCck gehabt.");
-					}
-					Spiel.Status();
 				}
 			}
-
+			if (zufallGen(feuerZufall) >= 35) {
+						System.out.println("Der Held wurde verletzt.");
+						hLebenspunkte -= feuer;
+					} else {
+						System.out.println("Der Drache hatte eine Fehlz\u00FCndung. Gl\u00FCck gehabt.");
+					}
+					status();
 		}
 
 		// Das Ergibnis des Kampfs.
-		if (Spiel.dLebenspunkte <= 0 && Spiel.hLebenspunkte <= 0 ) {
-			System.out.println("Weder der Drache noch der Held konnte die Schlacht überleben.");
-		} else if (Spiel.hLebenspunkte <= 0 ) {
+		if (dLebenspunkte <= 0 && hLebenspunkte <= 0 ) {
+			System.out.println("Weder der Drache noch der Held konnte die Schlacht \u00FCberleben.");
+		} else if (hLebenspunkte <= 0 ) {
 			System.out.println("Der Held wurde get\u00F6tet :-(");
 		} else {
 			System.out.println("Der Drache wurde besiegt. Hurra.");
 		}
 	}
 
-
 	/**
-	 * Die Methode Status gibt aus,
+	 * Die Methode status gibt aus,
 	 * wie viele Lebenspunkte für den Helden
 	 * und den Drachen übrig sind.
 	 */
-	public void Status() {
-		int hZaehler = hLebenspunkte;
-		int dZaehler = dLebenspunkte;
-
+	public static void status() {
 		System.out.print("Leben des Helden  : ");
-		while (hZaehler > 0) {
+		for (int hZaehler = hLebenspunkte; hZaehler > 0; hZaehler--) {
 			System.out.print("O ");
-			hZaehler--;
 		}
 
 		System.out.print("\nLeben des Drachen : ");
-		while (dZaehler > 0) {
+		for (int dZaehler = dLebenspunkte; dZaehler > 0; dZaehler--) {
 			System.out.print("X ");
-			dZaehler--;
 		}
-
+		
 		System.out.println("\n");
 	}
 
-
 	/**
-	 * Die Funktion ZufallGen nutzt die ZufallZahl einer
-	 * Waffe, um eine Trefferwahrscheinlichkeit zu generieren.
+	 * Die Funktion zufallGen nutzt die zufallZahl einer
+	 * waffe, um eine Trefferwahrscheinlichkeit zu generieren.
+	 * @param  zufallZahl Die Genauigkeit einer Waffe
+	 * @return            Gibt die generierte Zufallzahl zurück
 	 */
-	public int ZufallGen(int ZufallZahl) {
-		return (int) (Math.random() * ZufallZahl);
+	public static int zufallGen(int zufallZahl) {
+		return (int) (Math.random() * zufallZahl);
 	}
 }
