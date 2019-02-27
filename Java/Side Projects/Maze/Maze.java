bd1e6496c2bf072d5b9ab454264e8ad4cf09d9e5
Ahmad Al-Mekhlafy <ahmad.almekhlafy@gmail.com>
2019-02-27 13:21:14 +0100

    Done testing!

--- Java/Side Projects/Maze/Maze.java
+++ Java/Side Projects/Maze/Maze.java
@@ -20,10 +20,6 @@ class Maze extends JPanel implements ActionListener {
     static Random r = new Random();
     int startX = r.nextInt(width / cellSize);
     int startY = r.nextInt(height / cellSize);
-    //new branch;
-    //master;
-    //shitshitishf;
-    //tetsef;
     public static void main(String[] args) {
         new Maze();
     }
